40d28a49f4c8ec0b7672bbacc305dcc37fc62f91
화이팅 <somadubel@gmail.com>
2023-03-18 22:23:36 +0900

    feat: Draw rectangle using EBO

--- src/context.cpp
+++ src/context.cpp
@@ -10,27 +10,40 @@ ContextUPtr Context::Create()
 
 bool Context::Init()
 {
-	float vertices[] =
+    // 사각형의 코너 포인트 4개
+    float vertices[] = 
     {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f,
+         0.5f,  0.5f, 0.0f, // top right : 0
+         0.5f, -0.5f, 0.0f, // bottom right : 1
+        -0.5f, -0.5f, 0.0f, // bottom left : 2
+        -0.5f,  0.5f, 0.0f, // top left : 3
     };
 
-    // buffer object만들기 전 VAO 생성
+    uint32_t indices[] = // note that we start from 0!
+    { 
+        0, 1, 3, // first triangle
+        1, 2, 3, // second triangle
+    };
+
+    // VAO 생성 : 무조건 buffer object만들기 전에 생성
     glGenVertexArrays(1, &m_vertexArrayObject);
     glBindVertexArray(m_vertexArrayObject);
 
-    // vertex buffer 생성
+    // VBO 생성 : 
     glGenBuffers(1, &m_vertexBuffer);    
     // GL_ARRAY_BUFFER: vertex buffer object임. position, color등이 들어가는 buffer라고 명시해서 바인딩해주는 것
     glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer); 
-    // 바인딩되어 있는 버퍼에 데이터를 복사. 즉, 무조건 먼저 바인딩 해줘야 함.
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 9, vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 12, vertices, GL_STATIC_DRAW); // 바인딩되어 있는 버퍼에 데이터를 복사. 즉, 무조건 먼저 바인딩 해줘야 함.
 
+    // VAO attribute setting : 
     glEnableVertexAttribArray(0); // 0번 arrtribute 사용
     // 0번 arrtibute는 값이 3개고, float값이고, normalizing 할 필요없고, stride의 크기 설정하고, offset은 0부터 시작한다.
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0); 
+
+    // EBO : 어차피 인덱스는 정수값이 들어올 것이라고 알고 있으니까, 따로 VAO 세팅을 할 필요가 없다.
+    glGenBuffers(1, &m_indexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_indexBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint32_t) * 6, indices, GL_STATIC_DRAW);
 
     ShaderPtr vertShader = Shader::CreateFromFile("./shader/simple.vs", GL_VERTEX_SHADER);
     ShaderPtr fragShader = Shader::CreateFromFile("./shader/simple.fs", GL_FRAGMENT_SHADER);
@@ -56,5 +69,5 @@ void Context::Render()
     glClear(GL_COLOR_BUFFER_BIT);
 
     glUseProgram(m_program->Get());
-    glDrawArrays(GL_TRIANGLES, 0, 3); // 점 3개를 그려라.
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // incides가 uint이므로 uint로 설정
 }
