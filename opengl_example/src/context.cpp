fc3f8fe2d79073489e3da9f8c305fb730926e7b2
JunsuPark <somadubel@gmail.com>
2025-05-25 15:17:27 +0900

    [Feat] Render 함수를 색깔 애니메이션 형식으로 변경

--- opengl_example/src/context.cpp
+++ opengl_example/src/context.cpp
@@ -25,7 +25,7 @@ bool Context::Init()
     m_vertexLayout = VertexLayout::Create();
 
     m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW, vertices, sizeof(float) * 12);
-	m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+    m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
     m_indexBuffer = Buffer::CreateWithData(GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW, indices, sizeof(uint32_t) * 6);
 
     ShaderPtr vertShader = Shader::CreateFromFile("./shader/simple.vs", GL_VERTEX_SHADER);
@@ -39,9 +39,9 @@ bool Context::Init()
     if (!m_program)
         return false;
     SPDLOG_INFO("program id: {}", m_program->Get());
-    
+
     // 셰이더 내부 코드를 고치지 않고도 CPP코드에서 값을 세팅할 수 있음
-	auto loc = glGetUniformLocation(m_program->Get(), "color"); // 여기서 문자열 color는 simple.fs의 unfirom 변수명을 뜻함
+    auto loc = glGetUniformLocation(m_program->Get(), "color"); // 여기서 문자열 color는 simple.fs의 unfirom 변수명을 뜻함
     m_program->Use();
     glUniform4f(loc, 0.0f, 1.0f, 0.0f, 1.0f);
 
@@ -54,6 +54,12 @@ void Context::Render()
 {
     glClear(GL_COLOR_BUFFER_BIT);
 
+    static float time = 0.0f;
+    float t = sinf(time) * 0.5f + 0.5f; // -1.0 ~ 1.0 범위를 0.0 ~ 1.0으로 변환
+    auto loc = glGetUniformLocation(m_program->Get(), "color");
     m_program->Use();
+    glUniform4f(loc, t * t, 2.0f * t * (1.0f - t), (1.0f - t) * (1.0f - t), 1.0f);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    time += 0.016f;
 }
